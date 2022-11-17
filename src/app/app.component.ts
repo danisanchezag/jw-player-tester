4f3b2023c8be20d7675154c9ca7f4f4784fc369c
Daniela Sanchez <daniela.sancheza@perficient.com>
2022-11-16 19:09:17 -0500

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+declare var jwplayer: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-jwplayer';
-}
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    const playerJw = jwplayer('player').setup({
+      title: 'Player Test',
+      playlist: {
+        sources: [
+          {
+            file: 'https://dev-static.safefleetcloud.com/apps/media-management/dani-video.mp4',
+            type: 'mp4',
+          },
+        ],
+      },
+      width: 640,
+      height: 360,
+      aspectratio: '16:9',
+      mute: false,
+      autostart: true,
+      primary: 'html5',
+      hlshtml: true,
+    });
+  }
+  }
